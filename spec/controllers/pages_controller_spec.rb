baf74d5bcaa62d25eb1950643e803fdbf161616b
Nathaniel Ng <nng@cs.utsa.edu>
2011-11-08 10:46:54 -0600

    Added tests and title page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,19 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do 
+    get 'home'
+    response.should have_selector("title", :content => "Home")
+    end
+  end
+  
+  
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
   end
 
   describe "GET 'about'" do
