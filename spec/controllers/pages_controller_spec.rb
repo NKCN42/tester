8f4983bae9ee2eb1c55803334d6cff0c10ae6eb4
Nathaniel Ng <nng@cs.utsa.edu>
2011-11-08 10:31:54 -0600

    Added about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,9 +9,9 @@ describe PagesController do
     end
   end
 
-  describe "GET 'contact'" do
+  describe "GET 'about'" do
     it "returns http success" do
-      get 'contact'
+      get 'about'
       response.should be_success
     end
   end
